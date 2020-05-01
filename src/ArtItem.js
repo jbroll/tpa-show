00d2ea1ca5acccd82b588e99d72f5c495674bc32
Meghna Malhotra <m_malhotra@protonmail.com>
2020-05-01 13:21:34 -0700

    Add styles to art item component

--- src/ArtItem.js
+++ src/ArtItem.js
@@ -40,6 +40,16 @@ const useStyles = makeStyles((theme) => ({
         maxWidth: '100%',
         maxHeight: '100%',
     },
+    divStyle: {
+      display: 'flex',
+      flexDirection: 'row'
+    },
+    titleText: {
+      width: '66%'
+    },
+    imageBox: {
+      margin: 10
+    },
 }));
 
 export default function ArtItem(props) {
@@ -49,63 +59,62 @@ export default function ArtItem(props) {
 
   };
 
-  return <div>
-      <Grid container direction="row" spacing={1}>
-        <Grid item xs={2}>
-            <Button className={classes.image}>
-            <img className={classes.img} src="HainesFallsClove.jpg" alt="Drop Image Here"/>
-            </Button>
-        </Grid>
-        <Grid item container direction="column" spacing={1} xs={10}>
-            <Grid item xs={10}>
-                <TextField
-                    onChange={handleTitleChange}
-                    margin="dense"
-                    id={"title" + props.n}
-                    label="Title"
-                    type="text"
-                    value="4"
-                />
-            </Grid>
-            <Grid item container direction="row" spacing={1} xs={10}>
-                    <Grid item xs={2}>
-                        <TextField
-                            onChange={handleTitleChange}
-                            margin="dense"
-                            id={"media" + props.n}
-                            label="Media"
-                            type="text"
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <TextField
-                            onChange={handleTitleChange}
-                            margin="dense"
-                            id={"height" + props.n}
-                            label="Height"
-                            type="text"
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <TextField
-                            onChange={handleTitleChange}
-                            margin="dense"
-                            id={"width" + props.n}
-                            label="Width"
-                            type="text"
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <TextField
-                            onChange={handleTitleChange}
-                            margin="dense"
-                            id={"price" + props.n}
-                            label="Price"
-                            type="text"
-                        />
-                    </Grid>
-                </Grid>
-        </Grid>
-      </Grid>
-      </div>;
+  return <div className = {classes.divStyle}>
+           <Grid container direction="row" xs={2} className={classes.imageBox}>
+             <Grid item xs={2}>
+               <Button className={classes.image}>
+                 <img className={classes.img} src="HainesFallsClove.jpg" alt="Drop Image Here"/>
+               </Button>
+             </Grid>
+           </Grid>
+           <Grid container direction="row" spacing={1} xs={12}>
+             <Grid item xs={12}>
+               <TextField
+                 onChange={handleTitleChange}
+                 margin="dense"
+                 id={"title" + props.n}
+                 label="Title"
+                 type="text"
+                 value="4"
+                 className={classes.titleText}
+               />
+             </Grid>
+             <Grid item xs={2}>
+               <TextField
+                 onChange={handleTitleChange}
+                 margin="dense"
+                 id={"media" + props.n}
+                 label="Media"
+                 type="text"
+               />
+             </Grid>
+             <Grid item xs={2}>
+               <TextField
+                 onChange={handleTitleChange}
+                 margin="dense"
+                 id={"height" + props.n}
+                 label="Height"
+                 type="text"
+               />
+             </Grid>
+             <Grid item xs={2}>
+               <TextField
+                 onChange={handleTitleChange}
+                 margin="dense"
+                 id={"width" + props.n}
+                 label="Width"
+                 type="text"
+               />
+             </Grid>
+             <Grid item xs={2}>
+               <TextField
+                 onChange={handleTitleChange}
+                 margin="dense"
+                 id={"price" + props.n}
+                 label="Price"
+                 type="text"
+               />
+             </Grid>
+           </Grid>
+         </div>;
 }
