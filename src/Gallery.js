b5b9f8ea3f59e68182842951f7b5fe5d74eb66f4
John Roll <john@rkroll.com>
2020-05-03 20:57:05 -0400

    oops - ScaledImage was not really used

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Fade from '@material-ui/core/Fade';
+import ScaledImage from './ScaledImage';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({    
@@ -19,27 +20,6 @@ const useStyles = makeStyles((theme) => ({
         width: "100%"
     }
 }));
-
-export function ScaledImage(props) { 
-    const classes = useStyles();
-    const transparentPixel="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
-
-    function handleLoad(e) {
-        const img = e.currentTarget;
-        if (img.src === transparentPixel) {
-            return;
-        }
-        img.style.background="no-repeat url("+ img.src +") 50%";
-        img.style.backgroundSize="contain";  // Use "contain", "cover" or a % value
-        img.style.position="absolute";
-        img.style.width="100%";
-        img.style.height="100%";
-        img.src=transparentPixel;
-    }
-
-    // eslint-disable-next-line jsx-a11y/alt-text
-    return <img classes={classes.img} onLoad={handleLoad} {...props} />;
-}
 
 export default function Gallery(props) { 
     const classes = useStyles();
