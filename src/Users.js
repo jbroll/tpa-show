a26a5243d3f560a2213495ee0e7cccffd062800f
John Roll <john@rkroll.com>
2020-05-12 22:30:47 -0400

    Delete and Create User

--- src/Users.js
+++ src/Users.js
@@ -12,6 +12,8 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({    
@@ -36,6 +38,9 @@ export default function Users(props) {
     const classes = useStyles();
     const [openArtEntry, setOpenArtEntry] = React.useState(false);
     const [artEntry, setArtEntry] = React.useState("")
+    const [openDeleteUser, setOpenDeleteUser] = React.useState(false);
+    const [user, setUser] = React.useState(false);
+    const [openCreateUser, setOpenCreateUser] = React.useState(false);
 
     const handleOpenEntryOf = (uid) => {
         setOpenArtEntry(true);
@@ -50,6 +55,28 @@ export default function Users(props) {
         props.setClaim(uid, claim, value);
     };
 
+    const handleOpenDeleteUser = (user) => {
+        setUser(user);
+        setOpenDeleteUser(true);
+    }
+    const handleCloseDeleteUser = () => {
+        setOpenDeleteUser(false);
+    }
+    const handleDeleteUser = (uid) => {
+        props.deleteUser(uid);
+    }
+
+    const handleOpenCreateUser = (user) => {
+        setOpenCreateUser(true);
+    }
+    const handleCloseCreateUser = () => {
+        setOpenCreateUser(false);
+    }
+
+    const handleCreateUser = (uid) => {
+        props.createUser(uid);
+    }
+
     const users = props.users;
 
     return (
@@ -60,6 +87,11 @@ export default function Users(props) {
                     <TableCell>User</TableCell>
                     <TableCell>Registered</TableCell>
                     <TableCell>Admin</TableCell>
+                    <TableCell>
+                        <Button onClick={handleOpenCreateUser}>
+                            <AddCircleTwoToneIcon/>
+                        </Button>
+                    </TableCell>
                   </TableRow>
               </TableHead>
               <TableBody>
@@ -79,21 +111,43 @@ export default function Users(props) {
                                     onChange={e => { handleSetClaim(user.uid, 'adm', e.target.checked)}} 
                                     name="admin" inputProps={{ 'aria-label': 'Admin' }} />
                     </TableCell>
+                    <TableCell>
+                        <Button onClick={e => {handleOpenDeleteUser(user)}} >
+                            <DeleteIcon />
+                        </Button>
+                    </TableCell>
                   </TableRow>
               )}
               </TableBody>
           </Table>
           <Dialog   open={openArtEntry} maxWidth="md" fullWidth={true} 
                     onClose={handleCloseArtEntry} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Artist - {artEntry.email}</DialogTitle>
-            <DialogContent>
-                <ArtEntry uid={artEntry.uid} />
-            </DialogContent>
-            <DialogActions>
+            <DialogTitle id="form-dialog-title">Artist - {artEntry.email}
                 <Button onClick={handleCloseArtEntry} color="primary">
                     Close
                 </Button>
-            </DialogActions>
+            </DialogTitle>
+            <DialogContent>
+                <ArtEntry uid={artEntry.uid} />
+            </DialogContent>
+          </Dialog>
+          <Dialog   open={openDeleteUser} maxWidth="md" 
+                    onClose={handleCloseDeleteUser} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Delete User - {user.email}
+            </DialogTitle>
+            <DialogContent>
+                Are you sure you want to delete {user.email}?
+                <Button onClick={handleCloseDeleteUser}>Cancel</Button>
+                <Button onClick={e => { handleDeleteUser(user.uid)}}>Delete</Button>
+            </DialogContent>
+          </Dialog>
+          <Dialog   open={openCreateUser} maxWidth="md" 
+                    onClose={handleCloseCreateUser} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Create User</DialogTitle>
+            <DialogContent>
+                <Button onClick={handleCloseCreateUser}>Cancel</Button>
+                <Button onClick={e => { handleCreateUser(user.uid)}}>Create</Button>
+            </DialogContent>
           </Dialog>
         </div>
     );
