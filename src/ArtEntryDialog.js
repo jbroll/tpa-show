490c2a7e60b18145a5454aab8f429b7121e7e5a6
John Roll <john@rkroll.com>
2020-06-24 07:58:36 -0400

    ArtEntryDailog refactor and as admin access from ArtCatalog

---
+++ src/ArtEntryDialog.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import ArtEntry from './ArtEntry';
+import OkButton from './OkButton';
+import RDialog from './RDialog';
+
+export default function ArtEntryDialog(props) {
+
+    return (
+      <RDialog open={props.open} onClose={props.onClose} >
+        <DialogTitle >Twilight Park Art Show Entry - {props.title}
+            <OkButton onClick={props.onClose}/>
+        </DialogTitle>
+        <DialogContent>
+          <ArtEntry uid={props.uid}/>
+        </DialogContent>
+      </RDialog>
+    );
+}
